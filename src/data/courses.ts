aab2557bc98035fce05f35a50dd63a6581b1ded7
Mansour Lahreche <mansour.lahreche@etu.univ-mosta.dz>
2025-12-13 01:25:34 +0100

    Update courses.ts

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -19,7 +19,7 @@ export const courses: Course[] = [
     descKey: 'course.firstaid.desc',
     icon: HeartPulse,
     duration: 1,
-    image: '../src/data/First Aid.jpg',
+    image: '../src/data/FirstAid2.jpg',
     color: 'from-red-500 to-rose-500',
   },
   {
